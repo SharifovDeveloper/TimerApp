4dac8de53a6134f754add799c0c2180b7ab471bd
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-07-20 18:02:53 +0500

    Created Timer class

---
+++ TimerApp/Data/Timer.cs
@@ -0,0 +1,9 @@
+﻿namespace TimerApp.Data
+{
+    public class Timer
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
